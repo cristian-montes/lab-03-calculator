1052b6d56c485dd99e2915372918af5bac5bc1b9
Cristian Montes <cmontesmujica@gmail.com>
2021-06-03 14:58:24 -0700

    add feteaure working

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 // import functions
+import { add } from '../calculations.js';
 
 // reference needed DOM elements
 const addXInput = document.getElementById('add-x-input');
@@ -10,6 +11,12 @@ const addResult = document.getElementById('add-result');
 
 // set event listeners 
 addBtn.addEventListener('click', () => {
+
+    const x = Number(addXInput.value);
+    const y = Number(addYInput.value);
+    const sum = add(x, y);
+    addResult.textContent = sum;
+
   // do any needed work with the value(s)
   // update DOM to reflect new value(s)
   // get user input(s)
