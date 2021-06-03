c4ecbfb3549132b89ac0a9d4e68a08b6ae1e7f23
Cristian Montes <cmontesmujica@gmail.com>
2021-06-03 14:12:43 -0700

    add button clicked

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 // import functions
 
 // reference needed DOM elements
+const addXInput = document.getElementById('add-x-input');
+const addYInput = document.getElementById('add-y-input');
+const addBtn = document.getElementById('add-btn');
+const addResult = document.getElementById('add-result');
+
 
 
 // set event listeners 
+addBtn.addEventListener('click', () => {
+});
   // get user input(s)
   // do any needed work with the value(s)
   // update DOM to reflect new value(s)
+  
